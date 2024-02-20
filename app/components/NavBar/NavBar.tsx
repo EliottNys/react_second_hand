da1553cebab60f60187830733e71a9ff5096f350
Eliott Nys <eliott.nys@gmail.com>
2024-02-20 15:07:24 +0100

    added nextui navbar

--- app/components/NavBar/NavBar.tsx
+++ app/components/NavBar/NavBar.tsx
@@ -1,11 +1,17 @@
 import NavButton from "../NavButton/NavButton";
+import { Navbar, NavbarBrand, NavbarContent } from "@nextui-org/react";
 
 export default function NavigationBar() {
   return (
-    <div>
-      <NavButton title="Home" />
-      <NavButton title="Categories" />
-      <NavButton title="Saved" />
-    </div>
+    <Navbar shouldHideOnScroll>
+      <NavbarBrand>
+        <p>INSERT LOGO HERE</p>
+      </NavbarBrand>
+      <NavbarContent justify="center">
+        <NavButton title="Home" link="home" />
+        <NavButton title="Categories" link="catgories" />
+        <NavButton title="Saved" link="saved" />
+      </NavbarContent>
+    </Navbar>
   );
 }
--- app/components/NavButton/NavButton.tsx
+++ app/components/NavButton/NavButton.tsx
@@ -1,3 +1,15 @@
-export default function NavButton({ title }) {
-  return <button>{title}</button>;
+import { NavbarItem } from "@nextui-org/react";
+import Link from "next/link";
+
+interface NavButtonProps {
+  title: string;
+  link: string;
+}
+
+export default function NavButton(props: NavButtonProps) {
+  return (
+    <NavbarItem>
+      <Link href={props.link}>{props.title}</Link>
+    </NavbarItem>
+  );
 }
