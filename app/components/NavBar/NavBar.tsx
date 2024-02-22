8a7d09da7021e8951a85d446c92a4f2f294e9582
Eliott Nys <eliott.nys@gmail.com>
2024-02-22 22:18:55 +0100

    added login button

--- app/components/NavBar/NavBar.tsx
+++ app/components/NavBar/NavBar.tsx
@@ -1,18 +1,35 @@
+import Link from "next/link";
 import { Logo } from "../Logo/Logo";
 import NavButton from "../NavButton/NavButton";
-import { Navbar, NavbarBrand, NavbarContent } from "@nextui-org/react";
+import {
+  Button,
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+} from "@nextui-org/react";
 
 export default function NavigationBar() {
   return (
-    <Navbar shouldHideOnScroll>
+    <Navbar shouldHideOnScroll isBordered className="rounded-lg bg-blue">
       <NavbarBrand>
         <Logo />
         <p className="font-bold text-inherit">Second Life</p>
       </NavbarBrand>
-      <NavbarContent justify="center">
-        <NavButton title="Home" link="home" />
+      <NavbarContent className="hidden sm:flex gap-4" justify="center">
+        <NavButton title="Home" link="home" current={true} />
         <NavButton title="Categories" link="catgories" />
         <NavButton title="Saved" link="saved" />
+      </NavbarContent>
+      <NavbarContent justify="end">
+        <NavbarItem className="hidden lg:flex">
+          <Link href="#">Login</Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Button as={Link} color="primary" href="#" variant="flat">
+            Sign Up
+          </Button>
+        </NavbarItem>
       </NavbarContent>
     </Navbar>
   );
--- app/components/NavButton/NavButton.tsx
+++ app/components/NavButton/NavButton.tsx
@@ -4,12 +4,19 @@ import Link from "next/link";
 interface NavButtonProps {
   title: string;
   link: string;
+  current?: boolean;
 }
 
 export default function NavButton(props: NavButtonProps) {
   return (
     <NavbarItem>
-      <Link href={props.link}>{props.title}</Link>
+      <Link
+        href={props.link}
+        color="foreground"
+        aria-current={props.current ? "page" : "false"}
+      >
+        {props.title}
+      </Link>
     </NavbarItem>
   );
 }
