d9eaf22abe716f28c11d63d87d8f47b1d5a80736
Eliott Nys <eliott.nys@gmail.com>
2024-02-22 22:49:15 +0100

    made cards pressable

--- app/components/ItemCard/ItemCard.tsx
+++ app/components/ItemCard/ItemCard.tsx
@@ -10,14 +10,20 @@ interface ItemCardProps {
 
 export default function ItemCard(props: ItemCardProps) {
   return (
-    <Card className="py-4">
+    <Card isPressable className="py-4">
       <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
         <h1 className="font-bold text-large">{props.title}</h1>
         <h2 className="text-tiny uppercase font-bold">{props.price}</h2>
         <p className="text-default-500">{props.description}</p>
       </CardHeader>
       <CardBody className="overflow-visible py-2">
-        <Image alt="Item Picture" src={props.imgSrc} width={270} height={270} />
+        <Image
+          alt="Item Picture"
+          src={props.imgSrc}
+          width={270}
+          height={270}
+          className="object-cover rounded-xl"
+        />
       </CardBody>
     </Card>
   );
--- app/components/ItemList/ItemList.tsx
+++ app/components/ItemList/ItemList.tsx
@@ -2,24 +2,24 @@ import ItemCard from "../ItemCard/ItemCard";
 
 export default function ItemList() {
   return (
-    <div className="w-1/3">
+    <div>
       <ItemCard
         title="Fridge"
         price="100€"
         description="White fridge, good state but noisy"
-        imgSrc="/vercel.svg"
+        imgSrc="/plant.jpg"
       />
       <ItemCard
         title="Microwave"
         price="45€"
         description="700W Microwave, almost new."
-        imgSrc="/vercel.svg"
+        imgSrc="/plant.jpg"
       />
       <ItemCard
         title="Firewood"
         price="Best offer"
         description="Oak wood, 1m long, 30cm diameter, price negotiable. Cut in 2020."
-        imgSrc="/vercel.svg"
+        imgSrc="/plant.jpg"
       />
     </div>
   );
