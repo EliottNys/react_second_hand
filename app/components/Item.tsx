d3ba2df0ee5977c44fde65e5d141134f76d9cca7
Eliott Nys <eliott.nys@gmail.com>
2024-03-12 11:07:38 +0100

    styled and added props to item page

--- app/components/Item.tsx
+++ app/components/Item.tsx
@@ -2,6 +2,8 @@ import { Image } from "@nextui-org/react";
 
 interface ItemProps {
   title: string;
+  sellerName: string;
+  publishedDate: string;
   price: string;
   description: string;
   imgSrc: string;
@@ -9,11 +11,17 @@ interface ItemProps {
 
 export default function Item(props: ItemProps) {
   return (
-    <div>
+    <div className="grid grid-cols-2">
       <Image isZoomed src={props.imgSrc} alt="Item Picture" width={800} />
-      <h1>{props.title}</h1>
-      <p>{props.price}</p>
-      <p>{props.description}</p>
+      <div>
+        <h1 className="text-3xl pb-1">{props.title}</h1>
+        <p>
+          <span className="mx-2 font-semibold">{props.sellerName}</span>
+          <span className="mx-2 italic text-sm">{props.publishedDate}</span>
+        </p>
+        <p className="text-2xl text-red-500 py-3 font-bold">{props.price}</p>
+        <p className="pr-10">{props.description}</p>
+      </div>
     </div>
   );
 }
--- app/item/page.tsx
+++ app/item/page.tsx
@@ -5,6 +5,8 @@ export default function Page() {
     <div>
       <Item
         title="Houseplant assortment"
+        sellerName="Paul S."
+        publishedDate="12/03/2024"
         description="Beautiful assortment of houseplants, including Pachira aquatica Aubl., aloe vera, snake plant, and more. Perfect for indoor decoration."
         price="50€"
         imgSrc="/plant.jpg"
