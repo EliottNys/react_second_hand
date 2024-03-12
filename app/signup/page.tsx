58e0385668acd03e9f10f60ab067899f00341ba0
Eliott Nys <eliott.nys@gmail.com>
2024-03-12 14:25:02 +0100

    added sign up page

---
+++ app/signup/page.tsx
@@ -0,0 +1,23 @@
+import { Input } from "@nextui-org/react";
+
+export default function Page() {
+  return (
+    <div className="m-auto">
+      <h1 className="p-3 m-auto w-24">Sign Up:</h1>
+      <div className="p-3 grid grid-cols-2 gap-3">
+        <Input isRequired type="username" label="username">
+          username
+        </Input>
+        <Input isRequired type="email" label="email">
+          email
+        </Input>
+        <Input isRequired type="password" label="password">
+          password
+        </Input>
+        <Input isRequired type="repeat password" label="repeat password">
+          repeat password
+        </Input>
+      </div>
+    </div>
+  );
+}
