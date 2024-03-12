35f42721f911c87b725acbfe7a3e74b1ab7acc11
Eliott Nys <eliott.nys@gmail.com>
2024-03-12 14:32:59 +0100

    added links to login and signup page in navbar

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -17,16 +17,16 @@ export default function NavigationBar() {
         <p className="font-bold text-inherit">Second Life</p>
       </NavbarBrand>
       <NavbarContent className="hidden sm:flex gap-4" justify="center">
-        <NavButton title="Home" link="home" current={true} />
-        <NavButton title="Categories" link="catgories" />
+        <NavButton title="Home" link="." current={true} />
+        <NavButton title="Categories" link="categories" />
         <NavButton title="Saved" link="saved" />
       </NavbarContent>
       <NavbarContent justify="end">
         <NavbarItem className="hidden lg:flex">
-          <Link href="#">Login</Link>
+          <Link href="login">Login</Link>
         </NavbarItem>
         <NavbarItem>
-          <Button as={Link} color="primary" href="#" variant="flat">
+          <Button as={Link} color="primary" href="signup" variant="flat">
             Sign Up
           </Button>
         </NavbarItem>
