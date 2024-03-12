4669638f9ad1d0deb3f4e40eb18fd0a67682b1b2
Eliott Nys <eliott.nys@gmail.com>
2024-03-12 10:45:26 +0100

    added props to item page (and example page)

--- app/components/Item.tsx
+++ app/components/Item.tsx
@@ -1,3 +1,19 @@
-export default function Item() {
-  return <div></div>;
+import { Image } from "@nextui-org/react";
+
+interface ItemProps {
+  title: string;
+  price: string;
+  description: string;
+  imgSrc: string;
+}
+
+export default function Item(props: ItemProps) {
+  return (
+    <div>
+      <Image isZoomed src={props.imgSrc} alt="Item Picture" width={800} />
+      <h1>{props.title}</h1>
+      <p>{props.price}</p>
+      <p>{props.description}</p>
+    </div>
+  );
 }
--- app/item/page.tsx
+++ app/item/page.tsx
@@ -3,7 +3,12 @@ import Item from "../components/Item";
 export default function Page() {
   return (
     <div>
-      <Item />
+      <Item
+        title="Houseplant assortment"
+        description="Beautiful assortment of houseplants, including Pachira aquatica Aubl., aloe vera, snake plant, and more. Perfect for indoor decoration."
+        price="50€"
+        imgSrc="/plant.jpg"
+      />
     </div>
   );
 }
