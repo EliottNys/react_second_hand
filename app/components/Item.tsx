8281a7a6073f748e312d88d565b492ec124ba2bb
Eliott Nys <eliott.nys@gmail.com>
2024-03-12 13:15:35 +0100

    added padding to item information

--- app/components/Item.tsx
+++ app/components/Item.tsx
@@ -13,7 +13,7 @@ export default function Item(props: ItemProps) {
   return (
     <div className="grid grid-cols-2">
       <Image isZoomed src={props.imgSrc} alt="Item Picture" width={800} />
-      <div>
+      <div className="p-3">
         <h1 className="text-3xl pb-1">{props.title}</h1>
         <p>
           <span className="mx-2 font-semibold">{props.sellerName}</span>
