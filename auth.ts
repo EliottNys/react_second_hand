12f34ef5585028bb0c3eb184e424ee81e1d48676
Eliott Nys <eliott.nys@gmail.com>
2024-03-26 13:24:33 +0100

    modified auth config

--- auth.ts
+++ auth.ts
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
+import google from "next-auth/providers/google";
+import credentials from "next-auth/providers/credentials";
 
 export const {
   handlers: { GET, POST },
@@ -8,11 +9,25 @@ export const {
   signOut,
 } = NextAuth({
   providers: [
-    GoogleProvider({
+    google({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
+    credentials({
+      name: "credentials",
+      credentials: {
+        email: { label: "Email", type: "text" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials, req) {
+        // check in db
+        // okay ? => return user, something like this:
+        // const user = { id: "1", name: "J Smith", email: "jsmith@example.com" }
+        return null;
+      },
+    }),
   ],
+  session: { strategy: "jwt" }, //session is stored in cookie (default)
   callbacks: {
     async redirect(params) {
       const { url, baseUrl } = params;
