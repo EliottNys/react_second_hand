a0f83e223f21d68190d23bd97a6cb7f0e3450b30
Eliott Nys <eliott.nys@gmail.com>
2024-06-07 12:22:53 +0200

    added post, put and delete api endpoints for categories

--- app/api/categories/route.ts
+++ app/api/categories/route.ts
@@ -10,3 +10,49 @@ export async function GET(req: NextRequest) {
   const res = await getCategories();
   return Response.json(res);
 }
+
+export async function POST(req: NextRequest) {
+  const { name, superCategoryId } = await req.json();
+
+  try {
+    const newCategory = await prisma.category.create({
+      data: {
+        name,
+        superCategoryId,
+      },
+    });
+    return Response.json(newCategory);
+  } catch (error) {
+    return Response.json({ error: (error as Error).message }, { status: 400 });
+  }
+}
+
+export async function PUT(req: NextRequest) {
+  const { id, name, superCategoryId } = await req.json();
+
+  try {
+    const updatedCategory = await prisma.category.update({
+      where: { id },
+      data: {
+        name,
+        superCategoryId,
+      },
+    });
+    return Response.json(updatedCategory);
+  } catch (error) {
+    return Response.json({ error: (error as Error).message }, { status: 400 });
+  }
+}
+
+export async function DELETE(req: NextRequest) {
+  const { id } = await req.json();
+
+  try {
+    await prisma.category.delete({
+      where: { id },
+    });
+    return Response.json({ message: "Category deleted successfully" });
+  } catch (error) {
+    return Response.json({ error: (error as Error).message }, { status: 400 });
+  }
+}
